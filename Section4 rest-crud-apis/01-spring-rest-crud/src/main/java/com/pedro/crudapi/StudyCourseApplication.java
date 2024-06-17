afebdb5783e451de660fa029ce44126e03d2e90b
FreitasPedro <pedrospf2018@gmail.com>
2024-06-17 19:09:07 -0300

    API get Students

---
+++ Section4 rest-crud-apis/01-spring-rest-crud/src/main/java/com/pedro/crudapi/StudyCourseApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.crudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyCourseApplication.class, args);
+	}
+
+}
---
+++ Section4 rest-crud-apis/01-spring-rest-crud/src/test/java/com/pedro/crudapi/StudyCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.crudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
