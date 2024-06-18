2531f261669e07f8f702aabff26fbaf0af11c52c
FreitasPedro <pedrospf2018@gmail.com>
2024-06-18 17:03:37 -0300

    S3 - REST DAO ServiceLayer

---
+++ Section4 rest-crud-apis/02-spring-rest-crud-employee/src/main/java/com/pedro/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,12 @@
+package com.pedro.cruddemo.dao;
+
+import com.pedro.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+
+}
---
+++ Section4 rest-crud-apis/02-spring-rest-crud-employee/src/main/java/com/pedro/cruddemo/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.pedro.cruddemo.service;
+
+import com.pedro.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+}
