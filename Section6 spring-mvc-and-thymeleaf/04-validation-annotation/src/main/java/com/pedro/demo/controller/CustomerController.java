49f180871bdcb14f7bf36887398508960df5624f
FreitasPedro <pedrospf2018@gmail.com>
2024-06-22 17:01:07 -0300

    S6 Validation Number Range

--- Section6 spring-mvc-and-thymeleaf/04-validation-annotation/src/main/java/com/pedro/demo/controller/CustomerController.java
+++ Section6 spring-mvc-and-thymeleaf/04-validation-annotation/src/main/java/com/pedro/demo/controller/CustomerController.java
@@ -2,15 +2,28 @@ package com.pedro.demo.controller;
 
 import com.pedro.demo.entity.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // add an initbinder... to convert trim input strings
+    // remove leading and trailing whitespace
+    // resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model model) {
@@ -22,7 +35,12 @@ public class CustomerController {
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer customer,
                               BindingResult bindingResult){
-        if (bindingResult.hasErrors()) return "costumer-form";
+
+        System.out.println("Binding results: " + bindingResult.toString());
+        if (bindingResult.hasErrors()) {
+            System.err.println("Check errors Last name: |" + customer.getLastName() + "|");
+            return "customer-form";
+        }
         return "customer-confirmation";
     }
 }
--- Section6 spring-mvc-and-thymeleaf/04-validation-annotation/src/main/java/com/pedro/demo/entity/Customer.java
+++ Section6 spring-mvc-and-thymeleaf/04-validation-annotation/src/main/java/com/pedro/demo/entity/Customer.java
@@ -1,7 +1,6 @@
 package com.pedro.demo.entity;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 
 public class Customer {
@@ -10,7 +9,33 @@ public class Customer {
 
     @NotNull(message="is required")
     @Size(min=1, message="is required")
-    private String lastName = "";
+    private String lastName;
+
+    @NotNull
+    @Min(value = 0, message = "must be greater than or equal 0")
+    @Max(value = 10, message = "must be less than or equal 10")
+    private Integer freePasses;
+
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Only 5 chars/digits")
+    @Size(min = 5, max = 5)
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
