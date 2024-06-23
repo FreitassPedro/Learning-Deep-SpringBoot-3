6f112ac4fb625ea6eeb53b95af920fb68415e981
FreitasPedro <pedrospf2018@gmail.com>
2024-06-23 09:40:10 -0300

    S7 Update Button

--- Section7 crud-template/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/pedro/thymeleafdemo/controller/EmployeeController.java
+++ Section7 crud-template/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/pedro/thymeleafdemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.pedro.thymeleafdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -50,4 +47,13 @@ public class EmployeeController {
         return "redirect:/employees/list";
     }
 
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+        Employee emp = employeeService.findById(id);
+
+        model.addAttribute("employee", emp);
+
+        return "employees/employee-form";
+
+    }
 }
